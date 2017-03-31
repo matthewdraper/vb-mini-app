7aeebbc95cf06899c930c0a8ae23e2f0e8e5b79a
matthewdraper <matthewdraper@users.noreply.github.com>
2017-03-31 09:43:51 -0500

    Add Visual Studio project

---
+++ Lab7-600-Draper-Matthew/Lab7-600-Draper-Matthew/Lab7.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Lab7
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picRunning = New System.Windows.Forms.PictureBox()
+        Me.btnCalcLongTermPay = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.lblSalary = New System.Windows.Forms.Label()
+        Me.lblHourly = New System.Windows.Forms.Label()
+        Me.lblFuture = New System.Windows.Forms.Label()
+        Me.txtNumYears = New System.Windows.Forms.TextBox()
+        Me.lblYears = New System.Windows.Forms.Label()
+        Me.lstJobs = New System.Windows.Forms.ListBox()
+        Me.lblJobsAvailable = New System.Windows.Forms.Label()
+        CType(Me.picRunning, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picRunning
+        '
+        Me.picRunning.ErrorImage = Nothing
+        Me.picRunning.Image = Global.Lab7_600_Draper_Matthew.My.Resources.Resources.runninggraphic
+        Me.picRunning.Location = New System.Drawing.Point(12, 12)
+        Me.picRunning.Name = "picRunning"
+        Me.picRunning.Size = New System.Drawing.Size(364, 321)
+        Me.picRunning.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picRunning.TabIndex = 0
+        Me.picRunning.TabStop = False
+        '
+        'btnCalcLongTermPay
+        '
+        Me.btnCalcLongTermPay.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalcLongTermPay.Location = New System.Drawing.Point(46, 351)
+        Me.btnCalcLongTermPay.Name = "btnCalcLongTermPay"
+        Me.btnCalcLongTermPay.Size = New System.Drawing.Size(98, 46)
+        Me.btnCalcLongTermPay.TabIndex = 1
+        Me.btnCalcLongTermPay.Text = "Calculate long term pay"
+        Me.btnCalcLongTermPay.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(162, 351)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(98, 46)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClose.Location = New System.Drawing.Point(278, 351)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(98, 46)
+        Me.btnClose.TabIndex = 3
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'lblSalary
+        '
+        Me.lblSalary.AutoSize = True
+        Me.lblSalary.Font = New System.Drawing.Font("Palatino Linotype", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSalary.Location = New System.Drawing.Point(396, 237)
+        Me.lblSalary.Name = "lblSalary"
+        Me.lblSalary.Size = New System.Drawing.Size(101, 28)
+        Me.lblSalary.TabIndex = 4
+        Me.lblSalary.Text = "lblSalary"
+        '
+        'lblHourly
+        '
+        Me.lblHourly.AutoSize = True
+        Me.lblHourly.Font = New System.Drawing.Font("Palatino Linotype", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHourly.Location = New System.Drawing.Point(396, 265)
+        Me.lblHourly.Name = "lblHourly"
+        Me.lblHourly.Size = New System.Drawing.Size(110, 28)
+        Me.lblHourly.TabIndex = 5
+        Me.lblHourly.Text = "lblHourly"
+        '
+        'lblFuture
+        '
+        Me.lblFuture.AutoSize = True
+        Me.lblFuture.Font = New System.Drawing.Font("Palatino Linotype", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFuture.Location = New System.Drawing.Point(399, 368)
+        Me.lblFuture.Name = "lblFuture"
+        Me.lblFuture.Size = New System.Drawing.Size(96, 26)
+        Me.lblFuture.TabIndex = 6
+        Me.lblFuture.Text = "lblFuture"
+        '
+        'txtNumYears
+        '
+        Me.txtNumYears.Location = New System.Drawing.Point(553, 319)
+        Me.txtNumYears.Name = "txtNumYears"
+        Me.txtNumYears.Size = New System.Drawing.Size(64, 20)
+        Me.txtNumYears.TabIndex = 7
+        Me.txtNumYears.Visible = False
+        '
+        'lblYears
+        '
+        Me.lblYears.AutoSize = True
+        Me.lblYears.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblYears.Location = New System.Drawing.Point(398, 320)
+        Me.lblYears.Name = "lblYears"
+        Me.lblYears.Size = New System.Drawing.Size(149, 16)
+        Me.lblYears.TabIndex = 8
+        Me.lblYears.Text = "After so many years:"
+        '
+        'lstJobs
+        '
+        Me.lstJobs.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstJobs.FormattingEnabled = True
+        Me.lstJobs.ItemHeight = 25
+        Me.lstJobs.Items.AddRange(New Object() {"Desk Clerk", "Assistant Office Manager", "Running Specialist", "Inventory Clerk"})
+        Me.lstJobs.Location = New System.Drawing.Point(401, 114)
+        Me.lstJobs.Name = "lstJobs"
+        Me.lstJobs.Size = New System.Drawing.Size(298, 104)
+        Me.lstJobs.TabIndex = 9
+        '
+        'lblJobsAvailable
+        '
+        Me.lblJobsAvailable.Font = New System.Drawing.Font("Sitka Small", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblJobsAvailable.ForeColor = System.Drawing.Color.LightSeaGreen
+        Me.lblJobsAvailable.Location = New System.Drawing.Point(393, 13)
+        Me.lblJobsAvailable.Name = "lblJobsAvailable"
+        Me.lblJobsAvailable.Size = New System.Drawing.Size(324, 98)
+        Me.lblJobsAvailable.TabIndex = 10
+        Me.lblJobsAvailable.Text = "Jobs Available at our Running Store"
+        Me.lblJobsAvailable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Lab7
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(744, 414)
+        Me.Controls.Add(Me.lblJobsAvailable)
+        Me.Controls.Add(Me.lstJobs)
+        Me.Controls.Add(Me.lblYears)
+        Me.Controls.Add(Me.txtNumYears)
+        Me.Controls.Add(Me.lblFuture)
+        Me.Controls.Add(Me.lblHourly)
+        Me.Controls.Add(Me.lblSalary)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalcLongTermPay)
+        Me.Controls.Add(Me.picRunning)
+        Me.Name = "Lab7"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Available Jobs"
+        CType(Me.picRunning, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picRunning As PictureBox
+    Friend WithEvents btnCalcLongTermPay As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents lblSalary As Label
+    Friend WithEvents lblHourly As Label
+    Friend WithEvents lblFuture As Label
+    Friend WithEvents txtNumYears As TextBox
+    Friend WithEvents lblYears As Label
+    Friend WithEvents lstJobs As ListBox
+    Friend WithEvents lblJobsAvailable As Label
+End Class
---
+++ Lab7-600-Draper-Matthew/Lab7-600-Draper-Matthew/Lab7.vb
@@ -0,0 +1,106 @@
+﻿'   Created by Matthew Draper
+'   Last Edited On: 3/30/2017
+'   LaFreniere Running Store Lab 7
+'   Description: Takes input on form and calculates wages and stuffzzzz
+Option Strict On
+
+Public Class Lab7
+
+    Const cdecRaise As Decimal = 0.03D
+
+    ' Function takes the selected index of the listbox and returns the salary
+    Private Function FindYearlyPay(ByVal intSelectedIndex As Integer) As Decimal
+        Dim decDeskClerkPay As Decimal = 14900D
+        Dim decAssistantManagerPay As Decimal = 34430D
+        Dim decRunningSpecialistPay As Decimal = 28710D
+        Dim decInventoryClerk As Decimal = 12900D
+        Dim decSalary As Decimal = 0D
+
+        Select Case intSelectedIndex
+            Case 0
+                decSalary = decDeskClerkPay
+            Case 1
+                decSalary = decAssistantManagerPay
+            Case 2
+                decSalary = decRunningSpecialistPay
+            Case 3
+                decSalary = decInventoryClerk
+        End Select
+        Return decSalary
+    End Function
+    ' Function takes the selected index of the listbox and returns the hourly pay
+    Private Function FindHourlyPay(ByVal intSelectedIndex As Integer) As Decimal
+        Dim decDeskClerk As Decimal = 7.4D
+        Dim decAssistantManager As Decimal = 17.07D
+        Dim decRunningSpecialist As Decimal = 14.24D
+        Dim decInventoryClerk As Decimal = 6.39D
+        Dim decHourly As Decimal = 0D
+
+        Select Case intSelectedIndex
+            Case 0
+                decHourly = decDeskClerk
+            Case 1
+                decHourly = decAssistantManager
+            Case 2
+                decHourly = decRunningSpecialist
+            Case 3
+                decHourly = decInventoryClerk
+        End Select
+        Return decHourly
+    End Function
+    ' Event handler for the btnCalcLongTermPay click event
+    Private Sub btnCalcLongTermPay_Click(sender As Object, e As EventArgs) Handles btnCalcLongTermPay.Click
+        Dim decFutureSalary As Decimal
+        Dim intYears As Integer = 0
+        ' Try if txtNumYears can be parsed as an integer
+        Try
+            intYears = Integer.Parse(txtNumYears.Text)
+            ' If the years entered is out of range
+            If intYears < 1 Or intYears > 30 Then
+                MessageBox.Show("You need to enter years is not between 0 and 30.", "Format Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error)
+                txtNumYears.Focus()
+            Else ' Else the years entered is in range
+                Dim decSalary As Decimal = FindYearlyPay(lstJobs.SelectedIndex)
+                decFutureSalary = decSalary + (decSalary * cdecRaise * intYears)
+                lblFuture.Text = "Your future Salary will be $" & Format(decFutureSalary, "0.00")
+                lblFuture.Visible = True
+                btnClear.Enabled = True
+                btnCalcLongTermPay.Enabled = False
+            End If
+        Catch ex As Exception
+            MessageBox.Show("You need to enter years as a number.", "Format Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtNumYears.Focus()
+        End Try
+    End Sub
+    ' Event handler for the btnClear click event
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        btnCalcLongTermPay.Enabled = False
+        btnClear.Enabled = False
+        txtNumYears.Visible = False
+        lblSalary.ResetText()
+        lblHourly.ResetText()
+        lblFuture.ResetText()
+        lstJobs.Focus()
+    End Sub
+    ' Event handler for the btnClose click event
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Close()
+    End Sub
+    ' Event handler for the lstJobs changed selected index event
+    Private Sub lstJobs_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstJobs.SelectedIndexChanged
+        Dim intSelectedIndex As Integer = lstJobs.SelectedIndex()
+        Dim decYearlyPay As Decimal = FindYearlyPay(intSelectedIndex)
+        Dim decHourlyPay As Decimal = FindHourlyPay(intSelectedIndex)
+        lblSalary.Text = "Yearly Salary: $" & Format(decYearlyPay, "0.00")
+        lblHourly.Text = "Hourly Pay: $" & Format(decHourlyPay, "0.00")
+        btnCalcLongTermPay.Enabled = True
+        txtNumYears.Visible = True
+        txtNumYears.Focus()
+    End Sub
+    ' Clears the form on load by running a btnClear click event
+    Private Sub Lab7_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnClear_Click(sender, e)
+    End Sub
+End Class
